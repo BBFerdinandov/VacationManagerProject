e20d44bd5bab22a02699060a99812b8bff2b7823
BBFerdinandov18 <BBFerdinandov18@codingburgas.bg>
2022-11-17 14:27:47 +0200

    Generated Getters And Setters

--- src/main/java/com/project/VacationManagerProject/Users.java
+++ src/main/java/com/project/VacationManagerProject/Users.java
@@ -8,4 +8,67 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    String role;
+    String team;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
 }
